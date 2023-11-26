607c3ea7e6f8de15a645fa09623096c850575329
siyamar <ab.rahmansiyam@gmail.com>
2023-11-26 22:57:46 +0600

    added many requirds files

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h3>This is Home Page</h3>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+    return (
+        <div>
+            <h2>This is login</h2>
+        </div>
+    );
+};
+
+export default Login;
