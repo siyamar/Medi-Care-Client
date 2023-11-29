6bdc9007912fbaacfa72408acec2e42f919af9ed
siyamar <ab.rahmansiyam@gmail.com>
2023-11-29 16:41:45 +0600

    added firebase.config.js file, authProvider and helmet Provider

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwIyfNxJUaMt7hsuE_2BQXSs3JMKriPts",
+  authDomain: "medi-connect-92ee2.firebaseapp.com",
+  projectId: "medi-connect-92ee2",
+  storageBucket: "medi-connect-92ee2.appspot.com",
+  messagingSenderId: "81311886528",
+  appId: "1:81311886528:web:b251f4810a4a8442dd9125"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/Routes'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./providers/AuthProvider";
+import { HelmetProvider } from "react-helmet-async";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-    <div className="max-w-screen-xl mx-auto">
-    <RouterProvider router={router} />
-    </div>
-    </QueryClientProvider>
-  </React.StrictMode>,
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div className="max-w-screen-xl mx-auto">
+            <RouterProvider router={router} />
+          </div>
+        </HelmetProvider>
+      </QueryClientProvider>
+    </AuthProvider>
+  </React.StrictMode>
 );
